a104028ea8fce3f1b6ea6647f04dd59872dabc9c
tomdevos <tom@tomdevos.be>
2026-07-24 21:42:37 +0200

    Review App

--- app/database/migrations/2026_05_14_100400_create_gheops.php
+++ app/database/migrations/2026_05_14_100400_create_gheops.php
@@ -11,7 +11,7 @@ return new class extends Migration {
             $table->id();
             $table->unsignedTinyInteger('list_num');
             $table->string('nr');
-            $table->string('title');
+            $table->text('title');
             $table->text('comorbiditeit')->nullable();
             $table->text('rationale')->nullable();
             $table->text('alternatief')->nullable();
